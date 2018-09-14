cda64f00d8e2153e866f53a66794e87a8a0d10b5
Virginia Lopez-Nadal <vcl16@my.fsu.edu>
2018-09-13 21:32:07 -0400

    Assignment 4: First Commit - _Scence0 done

---
+++ Assets/Assignment 4/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+ 
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5f;
+   
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+ 
+        GetComponent<Rigidbody2D>().velocity = new Vector2(h, 0) * Speed;
+    }
+}
+
+
